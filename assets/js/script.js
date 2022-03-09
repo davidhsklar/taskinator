adfd46acb875e5951d3b72b3de7142aef91f4081
David Sklar <davidhsklar@gmail.com>
2022-03-09 16:00:09 -0500

    added innerHTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var formEl = document.querySelector("#task-form");
 
 var taskToDoEl = document.querySelector("#task-to-do");
 
+
 //creating list element
 
 var taskItemEl = document.createElement("li");
@@ -17,11 +18,32 @@ taskToDoEl.appendChild(taskItemEl);
 var createTaskHandler = function(event ) {
 
     event.preventDefault();
-    
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    taskToDoEl.appendChild(listItemEl);
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+
+    // create list item
+var listItemEl = document.createElement("li");
+
+listItemEl.className = "task-item";
+
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+
+// give it a class name
+
+taskInfoEl.className = "task-info";
+
+// add HTML content to div
+
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+// add entire list item to list
+
+taskToDoEl.appendChild(listItemEl);
   }
 
   //add task on form.  Because we're targeting the entire form instead of only the button, 
