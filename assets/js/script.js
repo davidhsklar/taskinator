f480670a9225fece68402508a8d820bac24ad306
David Sklar <davidhsklar@gmail.com>
2022-03-09 16:07:23 -0500

    refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,42 +13,45 @@ var taskItemEl = document.createElement("li");
 
 taskToDoEl.appendChild(taskItemEl);
 
-//applying task list formation to task items/table
 
-var createTaskHandler = function(event ) {
 
+var taskFormHandler = function(event) {
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+    // package up data as an object
 
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
+  
+    // send it as an argument to createTaskEl
 
-    // create list item
-var listItemEl = document.createElement("li");
-
-listItemEl.className = "task-item";
-
-// create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
-
-// give it a class name
-
-taskInfoEl.className = "task-info";
-
-// add HTML content to div
-
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-
-listItemEl.appendChild(taskInfoEl);
-
-// add entire list item to list
-
-taskToDoEl.appendChild(listItemEl);
+    createTaskEl(taskDataObj);
   }
 
+  var createTaskEl = function(taskDataObj) {
+    // create list item
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+    
+    // add entire list item to list
+    taskToDoEl.appendChild(listItemEl);
+
+}
   //add task on form.  Because we're targeting the entire form instead of only the button, 
   //we can't use the click event listener anymore. If we kept a click event listener, t
-  //then every time we clicked on the form it would run the createTaskHandler() function, 
+  //then every time we clicked on the form it would run the taskFormHandler () function, 
   // which isn't what we want. Instead, we'll use a form-specific event called submit
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler );
