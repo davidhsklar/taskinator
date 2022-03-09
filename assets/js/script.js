f7f897431224968ca6c9353ff1d926e123518e91
David Sklar <davidhsklar@gmail.com>
2022-03-09 13:27:27 -0500

    added task handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-script.js
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+var taskToDoEl = document.querySelector("#task-to-do");
+
+var taskItemEl = document.createElement("li");
+
+taskToDoEl.appendChild(taskItemEl);
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    taskToDoEl.appendChild(listItemEl);
+  }
+
+buttonEl.addEventListener("click", createTaskHandler);
